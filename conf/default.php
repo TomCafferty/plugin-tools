da492fcfa920e8401298e54e498678a693756ed7
TomCafferty <tcafferty@glocalfocal.com>
2012-07-22 16:44:31 -0400

    first commit of branch of tools to add csv & timeline

---
+++ conf/default.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Options for the BookmarkMe plugin
+ */
+
+$conf['show_tools'] = 1;
+$conf['show_link'] = 0;
+$conf['show_header'] = 1;
+$conf['tools'] = 'email,pdf,odt,print,csv,timeline';
+$conf['skip_ids'] = 'sidebar,user,group,playground,wiki:syntax';
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
+
